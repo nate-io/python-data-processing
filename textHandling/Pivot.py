366faf6090ab4e249927e6a8a3dcfd55e2ffed16
nate young <nyoung125@gmail.com>
2020-01-21 10:42:43 -0500

    add pivot table func

---
+++ textHandling/Pivot.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 10:20:25 2020
+
+@author: nate
+"""
+
+import os
+import pandas
+import numpy
+
+INPUT_DIR = "D:\python-course-files\exercises-output"
+INFILE = os.path.join(INPUT_DIR, "ConcatenatedMerged.csv")
+OUTFILE = os.path.join(INPUT_DIR, "Pivoted.csv")
+
+# aggregate data for analysis
+def pivot(infile = INFILE, outfile = OUTFILE):
+    df = pandas.read_csv(infile)
+    
+    """
+    data massage
+        - data provider annotates null dtaa with -9999, replace with NaN
+        - temp data is stored as value * 10 in order to not use floats, reconvert
+    """
+    df = df.replace(-9999, numpy.nan)
+    df["Temp"] = df["Temp"]/10.0
+    
+    """
+    aggregate data
+        - aggregating the ID (weather station)
+        - new columns are each year of data
+        - values filling the columns are the Temp column values
+    """
+    table = pandas.pivot_table(df, index=["ID"], columns="Year", values = "Temp")
+    table.to_csv(OUTFILE)
+    
